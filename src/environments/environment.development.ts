727536af55de1bf35a058d92caa07e9705b58d68
abel677 <abelandrade677@gmail.com>
2025-11-29 02:24:28 -0500

    actualizar labels vista caja

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:8000/api',
-  apiUrl: 'https://creditos-production-c314.up.railway.app/api',
+  apiUrl: 'http://localhost:8000/api',
+  //apiUrl: 'https://creditos-production-c314.up.railway.app/api',
 };
