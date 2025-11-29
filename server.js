e0ee3819ec89e7208f4a485a86eedb9d307e9b6d
abel677 <abelandrade677@gmail.com>
2025-11-29 00:26:29 -0500

    ajustar labels

--- server.js
+++ server.js
@@ -3,22 +3,16 @@ const path = require('path');
 
 const app = express();
 
-
-const PROJECT_NAME = 'web-creditos-dt';
+const PROJECT_NAME = 'web_creditos_dt';
 const DIST_FOLDER = path.join(process.cwd(), 'dist', PROJECT_NAME, 'browser');
 
-// Usar el puerto que proporciona Railway automáticamente (variable de entorno PORT)
 const PORT = process.env.PORT || 8080;
 
-// Servir la carpeta de archivos estáticos de Angular
 app.use(express.static(DIST_FOLDER));
-
-// Para el routing de Angular (todas las rutas no encontradas van a index.html)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(DIST_FOLDER, 'index.html'));
 });
 
-// Iniciar el servidor Express
 app.listen(PORT, () => {
   console.log(`Servidor Express escuchando en el puerto ${PORT}`);
 });
